d3efdfc56ef67fd0154af431a6a120730f50aeb9
Juan Carlos <juan.ramirezca28@gmail.com>
2023-03-24 15:59:18 -0600

    Adding a while loop to play at least 3 times

--- js/codigo.js
+++ js/codigo.js
@@ -2,21 +2,20 @@ function randomValue(min, max){
     return (Math.floor(Math.random() * (max - min + 1) + min)).toString()
 }
 
-function chosenValue(valor){
-    switch(valor){
-        case "1": 
-            return "📄";
-            break;
-        case "2":
-            return "🪨";
-            break;
-        case "3":
-            return "✂️";
-            break;
-        default:
-            return "opción no valida!";
-            break;
+function chosenValue(value){
+    let option = ""
+
+    if(value == "1"){
+        option = "📄";
+    } else if(value == "2"){
+        option = "🪨";
+    } else if(value == "3"){
+        option = "✂️";
+    } else {
+        option = "not a valid option!"
     }
+
+    return option
 }
 
 function resultOfgame(player, pc){
@@ -30,8 +29,10 @@ function resultOfgame(player, pc){
     3. scissors wons over paper
     */
    else if(player == "📄" && pc == "🪨" || player == "🪨" && pc == "✂️" || player == "✂️" && pc == "📄"){
+        wons += 1
         alert("You have won!!")  
    } else {
+        losses += 1
         alert("You lost!!")
    }
     
@@ -39,15 +40,22 @@ function resultOfgame(player, pc){
 
 }
 // #1 is for paper 📄, #2 is for  🪨, #3 is for ✂️
-let playerValue, pcValue, playerChoice, pcChoice;
+let playerValue, pcValue, playerChoice, pcChoice
+let wons = 0
+let losses = 0
 
-playerValue = prompt("Elige 1. para papel, 2. para piedra, 3 para tijera")
-playerChoice = chosenValue(playerValue)
-alert("You chose" + playerChoice)
+while(wons < 3 && losses <3){
+    playerValue = prompt("Choose #1 for 📄, #2 for 🪨, or #3 for ✂️")
+    playerChoice = chosenValue(playerValue)
+    alert("You chose" + playerChoice)
 
-pcValue = randomValue(1, 3);
-pcChoice = chosenValue(pcValue)
-alert("PC chose" + pcChoice)
+    pcValue = randomValue(1, 3);
+    pcChoice = chosenValue(pcValue)
+    alert("PC chose" + pcChoice)
 
-resultOfgame(playerChoice, pcChoice)
+    resultOfgame(playerChoice, pcChoice)
+}
+
+alert("You have won " + wons + " times and lost " + losses + " times.")
+
 
